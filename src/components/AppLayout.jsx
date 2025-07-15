f7a78f6c7243c93261aa263d84488dcc50ece973
ajay <ajaykumar290904@gmail.com>
2025-07-15 11:39:05 +0530

    fix: correct return statement formatting in AppLayout component

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -1,13 +1,14 @@
+import { Outlet } from "react-router-dom"
 import { Footers } from "./UI/Footers"
 import { Headers } from "./UI/Headers"
 
 
 export const AppLayout = () =>{
-    return 
+    return (
     <>
     <Headers/>
-
-
+    <Outlet />
     <Footers/>
     </>
+    )
 }
