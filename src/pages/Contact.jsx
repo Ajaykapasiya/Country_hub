1d50e31192c4d5f00bca6605f01578bac1a921c8
ajay <ajaykumar290904@gmail.com>
2025-07-16 00:21:12 +0530

    feat: implement Contact form with input fields for name, email, and message

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,36 @@
+export const Contact = () => {
+  return (
+    <section className="section-contact">
+      <h2 className="container-title"> Contact Us</h2>
 
+      <form action="">
+        <input
+          type="text"
+          required
+          autoComplete="false"
+          placeholder="Enter your name"
+          name="username"
+        />
 
-export const Contact = () =>{
-    return <h1>Contact page</h1>
-}
+        <input
+          type="email"
+          className="form-control"
+          autoComplete="false"
+          placeholder="Enter your Email"
+          name="email"
+          required
+        />
+
+        <textarea className="form-control"
+        rows="10" 
+        name="message" 
+        placeholder="Enter your message"
+        required
+        autoComplete="false"
+        ></textarea>
+
+        <button type="submit">Send</button>
+      </form>
+    </section>
+  );
+};
