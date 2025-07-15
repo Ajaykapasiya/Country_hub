02d88769f4dd9cf09105e78a9e5679751008d62c
ajay <ajaykumar290904@gmail.com>
2025-07-15 17:55:48 +0530

    feat: implement About page to display interesting facts about countries

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,38 @@
+import countryFacts from "../api/countryData.json";
 
+export const About = () => {
+  return (
+    <section className="section-about container">
+      <h2 className="container-title">
+        Here are the Interesting Facts
+        <br />
+        We're proud of
+      </h2>
 
-export const About = () =>{
-    return <h1>About page</h1>
-}
+      <div className="gradient-cards">
+        {countryFacts.map((country) => {
+            const {id,countryName,capital,population,interestingFact} = country
+          return (
+            <div className="cards" key={id}>
+              <div className="container-card bg-blue-box">
+                <p className="card-title">{countryName}</p>
+                <p>
+                  <span className="card-description">Captial:</span>
+                  {capital}
+                </p>
+                <p>
+                  <span className="card-description">Population:</span>
+                  {population}
+                </p>
+                <p>
+                  <span className="card-description">Interseting fact:</span>
+                  {interestingFact}
+                </p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
