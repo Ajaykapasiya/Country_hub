27e0f4e8a6c4423e3fcc6090e829cee5621d220b
ajay <ajaykumar290904@gmail.com>
2025-07-16 18:45:27 +0530

    feat: add SearchFilter component for searching and filtering countries

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -2,27 +2,62 @@ import { useEffect, useState, useTransition } from "react";
 import { getCountryData } from "../api/postApi";
 import { Loader } from "../components/UI/Loader";
 import { CountryCard } from "../components/Layout/CountryCard";
+import { SearchFilter } from "../components/UI/searchFilter";
 
 export const Country = () => {
   const [isPending, startTransition] = useTransition();
-  const [countries , setCountries]  = useState([]); 
+  const [countries, setCountries] = useState([]);
+
+  const [search, setSearch] = useState("");
+  const [filter, setFilter] = useState("all");
 
   useEffect(() => {
     startTransition(async () => {
       const res = await getCountryData();
-      
-      
-     setCountries(res.data)
+
+      setCountries(res.data);
     });
   }, []);
 
-  if (isPending) return <h1><Loader/></h1>;
+  if (isPending)
+    return (
+      <h1>
+        <Loader />
+      </h1>
+    );
 
-  return <section className="country-section">
-    <ul className="grid grid-four-cols">
-        {countries.map((curCountry ,index) => {
-return  <CountryCard country={curCountry}    key={index}     />})}
+      const searchCountry = (country) => {
+    if (search) {
+      return country.name.common.toLowerCase().includes(search.toLowerCase());
+    }
+    return country;
+  };
 
-    </ul>
-  </section>
+  const filterRegion = (country) => {
+    if (filter === "all") return country;
+    return country.region === filter;
+  };
+
+ 
+  const filterCountries = countries.filter(
+    (country) => searchCountry(country) && filterRegion(country)
+  );
+
+
+  return (
+    <section className="country-section">
+        <SearchFilter
+        search={search}
+        setSearch={setSearch}
+        filter={filter}
+        setFilter={setFilter}
+        />
+      <ul className="grid grid-four-cols">
+        {filterCountries.map((curCountry, index) => {
+          return <CountryCard country={curCountry} key={index} />;
+        })}
+      </ul>
+    </section>
+  );
+  
 };
