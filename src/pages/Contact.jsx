efd3c3c1aa85bb8e676b4fa92ee8eab675312f5f
ajay <ajaykumar290904@gmail.com>
2025-07-16 15:46:34 +0530

    feat: implement footer component with dynamic contact information and integrate into Home page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,36 +1,48 @@
 export const Contact = () => {
+    const handleFormSubmit = (formdata) => {
+console.log(formdata.entries());
+ const formInputData = Object.fromEntries(formdata.entries());
+ console.log(formInputData);
+ 
+
+
+    }
   return (
     <section className="section-contact">
       <h2 className="container-title"> Contact Us</h2>
 
-      <form action="">
-        <input
-          type="text"
-          required
-          autoComplete="false"
-          placeholder="Enter your name"
-          name="username"
-        />
+      <div className="contact-wrapper container">
+        <form action={handleFormSubmit}>
+          <input
+            type="text"
+            className="form-control"
+            required
+            autoComplete="false"
+            placeholder="Enter your name"
+            name="username"
+          />
 
-        <input
-          type="email"
-          className="form-control"
-          autoComplete="false"
-          placeholder="Enter your Email"
-          name="email"
-          required
-        />
+          <input
+            type="email"
+            className="form-control"
+            autoComplete="false"
+            placeholder="Enter your Email"
+            name="email"
+            required
+          />
 
-        <textarea className="form-control"
-        rows="10" 
-        name="message" 
-        placeholder="Enter your message"
-        required
-        autoComplete="false"
-        ></textarea>
+          <textarea
+            className="form-control"
+            rows="10"
+            name="message"
+            placeholder="Enter your message"
+            required
+            autoComplete="false"
+          ></textarea>
 
-        <button type="submit">Send</button>
-      </form>
+          <button type="submit">Send</button>
+        </form>
+      </div>
     </section>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import { HeroSection } from "../components/UI/HeroSection";
 import { About } from "./About";
+import { Contact } from "./Contact";
 
 export const Home = () => {
   return <>
   <HeroSection/>
   <About/>
+  <Contact/>
   </>
 }
 
